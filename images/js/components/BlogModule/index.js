1c092bd9946774c929c94bacd4d22f25250ed9bc
Valentin Yakovlev <yakovlev.valentin97@gmail.com>
2017-12-10 20:09:50 +0200

    merge is done

--- images/js/components/BlogModule/index.js
+++
@@ -1,9 +0,0 @@
-import BlogCtrl from 'BlogModule/Blog';
-import PostCtrl from 'BlogModule/Post';
-import PaginationCtrl from 'BlogModule/Pagination';
-
-export const BlogModule = {
-	blog: BlogCtrl,
-	post: PostCtrl,
-	pagination: PaginationCtrl
-};
--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 import angular from 'angular'
 import ngRoute from 'angular-route'
-import {BlogModule} from 'BlogModule'
+import {BlogModule} from 'components/BlogModule/index'
 import routerSettings from 'app.router.config'
 import blogApi from 'api/blogapi'
 import 'angular-aria'
 import 'angular-animate'
 import 'angular-material'
+import 'angular-messages'
 
 angular.module('application', ['ngRoute'])
  .config(routerSettings)
